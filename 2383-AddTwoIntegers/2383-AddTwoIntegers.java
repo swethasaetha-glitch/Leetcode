83006277f682eda20a7db6eafb42ff238fdab23c
swethasaetha-glitch <swethasaetha@gmail.com>
2026-06-04 21:42:09 +0530

    Create 2383-AddTwoIntegers

---
+++ 2383-AddTwoIntegers/2383-AddTwoIntegers.java
@@ -0,0 +1,15 @@
+// Last updated: 6/4/2026, 9:42:08 PM
+class Solution {
+    public int sum(int num1, int num2) {
+        return num1+num2;
+    }
+}
+public class Main{
+    public static void main(String[] args){
+        int num1=12,num2=5;
+        Solution Solution = new Solution();
+        int result=Solution.sum( num1,num2);
+        System.out.println(result);
+
+    }
+}
