9337fa84fcdac13bed4bd606b7147e52d9bad741
swethasaetha-glitch <swethasaetha@gmail.com>
2026-06-04 21:42:54 +0530

    Create 70-ClimbingStairs

---
+++ 70-ClimbingStairs/70-ClimbingStairs.java
@@ -0,0 +1,13 @@
+// Last updated: 6/4/2026, 9:42:53 PM
+class Solution {
+    public int climbStairs(int n) {
+        if(n<=2) return n;
+        int[] a = new int[n+1];
+        a[1]=1;
+        a[2]=2;
+        for(int i=3;i<=n;i++){
+            a[i]=a[i-1]+a[i-2];
+        }
+        return a[n];
+    }
+}
