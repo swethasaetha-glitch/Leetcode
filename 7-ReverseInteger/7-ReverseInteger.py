8da1c8c87733993b175724c44c1fa906a08f4ec5
swethasaetha-glitch <swethasaetha@gmail.com>
2026-06-09 14:26:05 +0530

    File updated at 6/9/2026, 2:26:03 PM

--- 7-ReverseInteger/7-ReverseInteger.py
+++ 7-ReverseInteger/7-ReverseInteger.py
@@ -1,12 +1,14 @@
-# Last updated: 6/9/2026, 2:12:09 PM
+# Last updated: 6/9/2026, 2:26:03 PM
 1class Solution:
-2    def reverse(self, x: int) -> int:
-3        if x < 0:
-4            rev = -int(str(-x)[::-1])
-5        else:
-6            rev = int(str(x)[::-1])
-7
-8        if rev < -2**31 or rev > 2**31 - 1:
-9            return 0
-10
-11        return rev
+2    def isHappy(self, n: int) -> bool:
+3        seen=set()
+4        while n!=1:
+5            if n in seen:
+6                return False
+7            seen.add(n)
+8            digits=str(n)
+9            t=0
+10            for i in digits:
+11                t+=int(i)**2
+12            n=t
+13        return True
