7408ce4718d4f1012c579c66181196e9eca058b3
swethasaetha-glitch <swethasaetha@gmail.com>
2026-06-09 14:10:06 +0530

    Create 7-ReverseInteger

---
+++ 7-ReverseInteger/7-ReverseInteger.py
@@ -0,0 +1,12 @@
+# Last updated: 6/9/2026, 2:10:04 PM
+1class Solution:
+2    def reverse(self, x: int) -> int:
+3        if x < 0:
+4            rev = -int(str(-x)[::-1])
+5        else:
+6            rev = int(str(x)[::-1])
+7
+8        if rev < -2**31 or rev > 2**31 - 1:
+9            return 0
+10
+11        return rev
