23ea3adba77c0f11907c626d4e9ca29f6e8d91c2
swethasaetha-glitch <swethasaetha@gmail.com>
2026-06-15 11:50:50 +0530

    File updated at 6/15/2026, 11:50:46 AM

--- 371-SumofTwoIntegers/371-SumofTwoIntegers.py
+++ 371-SumofTwoIntegers/371-SumofTwoIntegers.py
@@ -1,4 +1,4 @@
-# Last updated: 6/15/2026, 11:47:13 AM
+# Last updated: 6/15/2026, 11:50:46 AM
 1class Solution:
-2    def getSum(self, a: int, b: int) -> int:
-3        return a+b
+2    def containsDuplicate(self, nums: List[int]) -> bool:
+3        return len(nums) != len(set(nums))
