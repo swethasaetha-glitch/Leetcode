a2cf3b268e596eb9a3659da5f02158a4b67c835d
swethasaetha-glitch <swethasaetha@gmail.com>
2026-06-15 21:31:31 +0530

    File updated at 6/15/2026, 9:31:30 PM

--- 139-WordBreak/139-WordBreak.py
+++ 139-WordBreak/139-WordBreak.py
@@ -1,22 +1,11 @@
-# Last updated: 6/15/2026, 9:21:01 PM
+# Last updated: 6/15/2026, 9:31:30 PM
 1class Solution:
 2    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
-3        memo = {}
-4
-5        def solve(start):
-6            if start == len(s):
-7                return True
-8
-9            if start in memo:
-10                return memo[start]
-11
-12            for word in wordDict:
-13                if s.startswith(word, start):
-14                    if solve(start + len(word)):
-15                        memo[start] = True
-16                        return True
-17
-18            memo[start] = False
-19            return False
-20
-21        return solve(0)
+3        possible = {0}
+4        for i in range(len(s)):
+5            if i in possible:
+6                for word in wordDict:
+7                    if s.startswith(word, i):
+8                        possible.add(i + len(word))
+9
+10        return len(s) in possible
