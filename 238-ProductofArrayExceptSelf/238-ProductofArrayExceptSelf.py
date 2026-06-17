74a11d4e15909700933ffcc1eae4893dc92cd92a
swethasaetha-glitch <swethasaetha@gmail.com>
2026-06-17 11:48:35 +0530

    File updated at 6/17/2026, 11:48:32 AM

--- 238-ProductofArrayExceptSelf/238-ProductofArrayExceptSelf.py
+++ 238-ProductofArrayExceptSelf/238-ProductofArrayExceptSelf.py
@@ -1,11 +1,9 @@
-# Last updated: 6/17/2026, 11:38:29 AM
+# Last updated: 6/17/2026, 11:48:32 AM
 1class Solution:
-2    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-3        d={}
-4        for i in strs:
-5            s="".join(sorted(i))
-6            if s not in d:
-7                d[s]=[i]
-8            else:
-9                d[s].append(i)
-10        return list(d.values())
+2    def isValid(self, s: str) -> bool:
+3        while "()" in s or "{}" in s or "[]" in s:
+4            s = s.replace("()", "")
+5            s = s.replace("{}", "")
+6            s = s.replace("[]", "")
+7
+8        return s == ""
