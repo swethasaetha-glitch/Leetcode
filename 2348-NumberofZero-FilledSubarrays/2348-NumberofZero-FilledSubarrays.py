d00b42e4e97e0ce9738c8f0199edd967c6b1294e
swethasaetha-glitch <swethasaetha@gmail.com>
2026-06-22 14:24:39 +0530

    File updated at 6/22/2026, 2:24:39 PM

--- 2348-NumberofZero-FilledSubarrays/2348-NumberofZero-FilledSubarrays.py
+++ 2348-NumberofZero-FilledSubarrays/2348-NumberofZero-FilledSubarrays.py
@@ -1,16 +1,11 @@
-# Last updated: 6/22/2026, 2:05:43 PM
+# Last updated: 6/22/2026, 2:24:39 PM
 1class Solution:
-2    def subarraySum(self, nums: List[int], k: int) -> int:
-3        count = 0
-4        prefix = 0
-5        d = {0: 1}
-6
-7        for num in nums:
-8            prefix += num
+2    def findMaxAverage(self, nums: List[int], k: int) -> float:
+3        window = sum(nums[:k])
+4        ans = window
+5
+6        for i in range(k, len(nums)):
+7            window += nums[i] - nums[i-k]
+8            ans = max(ans, window)
 9
-10            if prefix - k in d:
-11                count += d[prefix - k]
-12
-13            d[prefix] = d.get(prefix, 0) + 1
-14
-15        return count
+10        return ans / k
