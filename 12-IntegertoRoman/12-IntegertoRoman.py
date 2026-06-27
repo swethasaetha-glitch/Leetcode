011a906f485beb988b66e4cfa6c487d3ad5b2172
swethasaetha-glitch <swethasaetha@gmail.com>
2026-06-27 10:21:41 +0530

    File updated at 6/27/2026, 10:21:47 AM

--- 12-IntegertoRoman/12-IntegertoRoman.py
+++ 12-IntegertoRoman/12-IntegertoRoman.py
@@ -1,16 +1,11 @@
-# Last updated: 6/27/2026, 10:14:19 AM
+# Last updated: 6/27/2026, 10:21:47 AM
 1class Solution:
 2    def intToRoman(self, num: int) -> str:
 3        values = [1000,900,500,400,100,90,50,40,10,9,5,4,1]
-4
-5        symbols = ["M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"]
-6
-7        ans = ""
-8
-9        for i in range(len(values)):
-10
-11            while num >= values[i]:
-12                ans += symbols[i]
-13                num -= values[i]
-14
-15        return ans
+4        symbols = ["M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"]
+5        ans = ""
+6        for i in range(len(values)):
+7            while num >= values[i]:
+8                ans += symbols[i]
+9                num -= values[i]
+10        return ans
