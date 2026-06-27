32b88412250d20f0ddd5d4f17c9f9932e7be4f77
swethasaetha-glitch <swethasaetha@gmail.com>
2026-06-27 11:48:54 +0530

    File updated at 6/27/2026, 11:49:01 AM

--- 152-MaximumProductSubarray/152-MaximumProductSubarray.py
+++ 152-MaximumProductSubarray/152-MaximumProductSubarray.py
@@ -1,18 +1,19 @@
-# Last updated: 6/27/2026, 11:37:54 AM
+# Last updated: 6/27/2026, 11:49:01 AM
 1class Solution:
 2    def countPrimes(self, n: int):
-3
-4        if n <= 2:
-5            return 0
-6
-7        prime = [True] * n
-8        prime[0] = prime[1] = False
-9
-10        for i in range(2, int(n ** 0.5) + 1):
-11
-12            if prime[i]:
-13
-14                for j in range(i * i, n, i):
-15                    prime[j] = False
-16
-17        return sum(prime)
+3        if n <= 2:
+4            return 0
+5
+6        prime = [True] * n
+7
+8        prime[0] = False
+9        prime[1] = False
+10
+11        for i in range(2, n):
+12
+13            if prime[i]:
+14
+15                for j in range(i * 2, n, i):
+16                    prime[j] = False
+17
+18        return prime.count(True)
