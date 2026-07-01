e8c8753e7a627cc9690a2999491333135a549ff0
swethasaetha-glitch <swethasaetha@gmail.com>
2026-07-01 11:09:02 +0530

    File updated at 7/1/2026, 11:08:59 AM

--- 41-FirstMissingPositive/41-FirstMissingPositive.py
+++ 41-FirstMissingPositive/41-FirstMissingPositive.py
@@ -1,4 +1,13 @@
-# Last updated: 7/1/2026, 10:15:22 AM
+# Last updated: 7/1/2026, 11:08:59 AM
 1class Solution:
-2    def reverseWords(self, s: str) -> str:
-3        return " ".join(s.split()[::-1])
+2    def climbStairs(self, n: int) -> int:
+3        if n <= 2:
+4            return n
+5        a = 1
+6        b = 2
+7        for i in range(3, n + 1):
+8            c = a + b
+9            a = b
+10            b = c
+11
+12        return b
